33eb554c63f693dc831d7163cac7cd468726063c
ABIZABI6453 <156945780+ABIZABI6453@users.noreply.github.com>
2024-01-17 22:27:32 +0100

    S

--- script.js
+++
@@ -1,8 +0,0 @@
-function goToTopic() {
-    var selectedTopic = document.getElementById('mathTopic').value;
-    if (selectedTopic) {
-        window.location.href = selectedTopic;
-    } else {
-        alert("Please select a math topic. Otherwise we can't do anything.");
-    }
-}
