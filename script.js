c8d4602c64f4f7edf127ffa9f53061dc0f674f61
ABIZABI6453 <156945780+ABIZABI6453@users.noreply.github.com>
2024-01-17 22:10:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,3 @@ function goToTopic() {
         alert("Please select a math topic. Otherwise we can't do anything.");
     }
 }
-
-
-function createSnowflake() {
-    const snowflake = document.createElement('div');
-    snowflake.classList.add('snowflake');
-    snowflake.style.left = Math.random() * 100 + 'vw';
-    snowflake.style.animationDuration = Math.random() * 3 + 2 + 's'; // Random animation duration
-    snowflake.style.opacity = Math.random();
-    snowflake.style.transform = `scale(${Math.random()})`;
-
-
-setInterval(createSnowflake, 100); // Adjust interval for more/less frequent snowflakes
