b15e63f690578796c4a0527ebfb4e6ed0ced63f0
ABIZABI6453 <156945780+ABIZABI6453@users.noreply.github.com>
2024-01-17 22:20:16 +0100

    Removed snowflakes once again

--- script.js
+++ script.js
@@ -7,20 +7,4 @@ function goToTopic() {
     }
 }
 
-//function createSnowflake() {
-    const snowflake = document.createElement('div');
-    snowflake.classList.add('snowflake');
-    snowflake.style.left = Math.random() * 100 + 'vw';
-    snowflake.style.animationDuration = Math.random() * 3 + 2 + 's'; // Random animation duration
-    snowflake.style.opacity = Math.random();
-    snowflake.style.transform = `scale(${Math.random()})`;
-
-    //document.getElementById('snowflakes-container').appendChild(snowflake);
-
-    // Remove snowflake after it falls
-    setTimeout(() => {
-        snowflake.remove();
-    }, 5000);
-}
-
 setInterval(createSnowflake, 100); // Adjust interval for more/less frequent snowflakes
