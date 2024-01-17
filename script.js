fd4d3c3e533eaaf8a5b178ec4d903a93e930185b
ABIZABI6453 <156945780+ABIZABI6453@users.noreply.github.com>
2024-01-17 22:22:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,3 @@ function goToTopic() {
         alert("Please select a math topic. Otherwise we can't do anything.");
     }
 }
-
-setInterval(createSnowflake, 100); // Adjust interval for more/less frequent snowflakes
